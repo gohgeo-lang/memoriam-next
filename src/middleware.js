9349b224ef2cfeea0dc2a50549c55cf0e7534b6b
gohgeo <gohgeo@gmail.com>
2025-11-03 17:06:44 +0900

    middleware

---
+++ src/middleware.js
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
+
+export async function middleware(req) {
+  const token = await getToken({
+    req,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
+
+  if (!token) {
+    const loginUrl = new URL("/login", req.url);
+    loginUrl.searchParams.set("from", req.nextUrl.pathname);
+    return NextResponse.redirect(loginUrl);
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/service/:path*"],
+};
