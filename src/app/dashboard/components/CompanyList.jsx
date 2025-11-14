804892b6b339e623b6f7f1fd80a8c577c886bdce
SIH <lordangel82@gmail.com>
2025-11-14 14:48:40 +0900

    배포관련 수정 및 관리자 페이지 업체화면한줄당 표시개수 수정

--- src/app/dashboard/components/CompanyList.jsx
+++ src/app/dashboard/components/CompanyList.jsx
@@ -10,7 +10,7 @@ export default function CompanyList({ data, onEdit }) {
   return (
     <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
       <div className="px-2 py-2 sm:px-0">
-        <div className="bg-gray-400 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 p-2 gap-1">
+        <div className="bg-gray-400 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 p-2 gap-1">
           {data.map((company) => (
             <div
               key={company.id}
